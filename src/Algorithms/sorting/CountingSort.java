d5866431bf9557acd9b0c2ad379f6857e47039c7
Anurag Singh Rajawat <anuragsinghrajawat22@gmail.com>
2022-01-21 20:07:08 +0530

    Implemented Radix and Shell sort algorithms

--- src/Algorithms/sorting/CountingSort.java
+++ src/Algorithms/sorting/CountingSort.java
@@ -6,11 +6,30 @@ import java.util.Arrays;
  * Time Complexity: O(n + k) where n is length of array, and k is input range
  * <p>Space complexity: O(n + k)</p>
  */
+
+// TODO: Modify it to sort negative numbers
 public class CountingSort {
     public static void main(String[] args) {
         int[] a = {1, 4, 1, 2, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 7, 5, 2};
+        int[] b = {55, 43, 1, 678};
+        int[] c = {1};
+        int[] d = {};
+        int[] e = {2, 1};
+        int[] f = {2, 0, 1, 2};
+
         sort(a, 10);
+        sort(b, 700);
+        sort(c, 10);
+        sort(d, 10);
+        sort(e, 10);
+        sort(f, 10);
+
         System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.toString(c));
+        System.out.println(Arrays.toString(d));
+        System.out.println(Arrays.toString(e));
+        System.out.println(Arrays.toString(f));
     }
 
     public static void sort(int[] a, int range) {

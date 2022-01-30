e3ea66ae8cf14f09da7d79b15e5a974aee8c6130
Anurag Singh Rajawat <anuragsinghrajawat22@gmail.com>
2022-01-30 21:57:14 +0530

    Implemented static, dynamic and circular queues

---
+++ src/datastructures/CircularQueueMain.java
@@ -0,0 +1,30 @@
+package datastructures;
+
+public class CircularQueueMain {
+    public static void main(String[] args) {
+        CircularQueue queue = new CircularQueue();
+        for (int i = 1; i <= 10; i++) {
+            queue.offer(i * i * i);
+        }
+        System.out.println("Size of queue is: " + queue.size());
+        System.out.println(queue);
+        System.out.println();
+//        for (int i = 0; i <= queue.size(); i++) {
+//            i = 0;
+//            System.out.println(queue.poll());
+//        }
+        System.out.println();
+        System.out.println(queue.poll());
+        queue.offer(1111);
+
+        System.out.println("Size of queue is: " + queue.size());
+        System.out.println(queue);
+
+        System.out.println();
+        System.out.println(queue.size());
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        queue.offer(123);
+        System.out.println(queue);
+    }
+}

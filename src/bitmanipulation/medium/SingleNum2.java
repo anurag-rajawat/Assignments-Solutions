9d8df978bdc5dc4ab2cb69b7d2a50c309e203bdb
Anurag Singh Rajawat <anuragsinghrajawat22@gmail.com>
2022-02-08 12:15:50 +0530

    Completed Bit manipulation assignment

---
+++ src/bitmanipulation/medium/SingleNum2.java
@@ -0,0 +1,23 @@
+package bitmanipulation.medium;
+
+import java.util.HashMap;
+
+// Problem 3: https://leetcode.com/problems/single-number-ii/
+public class SingleNum2 {
+    public static void main(String[] args) {
+        int[] a = {30000, 500, 100, 30000, 100, 30000, 100};
+        System.out.println(singleNumber(a));
+    }
+
+    // Using HashMap
+    public static int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) return key;
+        }
+        return -1;
+    }
+}
---
+++ src/bitmanipulation/medium/Subsets.java
@@ -0,0 +1,21 @@
+package bitmanipulation.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Problem 1: https://leetcode.com/problems/subsets/
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for (int num : nums) {
+            int size = outer.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}

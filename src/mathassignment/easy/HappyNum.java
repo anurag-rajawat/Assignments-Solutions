3ea7cfbb52b4a0731fe867cf5a7f04accf8bf903
Anurag Singh Rajawat <anuragsinghrajawat22@gmail.com>
2022-02-09 10:14:47 +0530

    Completed easy problems of math assignment

---
+++ src/mathassignment/easy/HappyNum.java
@@ -0,0 +1,27 @@
+package mathassignment.easy;
+
+// Problem 1: https://leetcode.com/problems/happy-number/
+public class HappyNum {
+    // This question uses fast and slow pointer approach of linked-list concepts under the hood.
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+
+        do{
+            slow=square(slow);
+            fast=square(square(fast));
+        }while(slow != fast);
+
+        return slow == 1;
+    }
+
+    private int square(int num){
+        int ans=0;
+        while(num>0){
+            int rem=num%10;
+            ans+=(rem*rem);
+            num/=10;
+        }
+        return ans;
+    }
+}
---
+++ src/mathassignment/easy/PowerOf2.java
@@ -0,0 +1,12 @@
+package mathassignment.easy;
+
+// Problem 11: https://leetcode.com/problems/power-of-two/
+// Same problem of recursion
+public class PowerOf2 {
+    public static boolean isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        if (n == 1) return true;
+        if (n % 2 != 0) return false;
+        return isPowerOfTwo(n / 2);
+    }
+}
